b4c9f3a4aadb65c701355b650b283b9028f69b17
Vaeliwen <zaman@imbriaco.com>
2019-06-12 18:38:46 -0400

    Completed addition form, began working on edit.

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import axios from 'axios';
+import ReactDOM from 'react-dom';
 
 
 
@@ -8,7 +9,11 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      friends: ['test']
+      friends: [],
+      name: null,
+      age: null,
+      email: null,
+      id: null
     } 
   }
 
@@ -26,6 +31,51 @@ class App extends React.Component {
         });
   };
 
+  nameHandler(event) {
+    this.setState({name: event.target.value})
+  }
+
+  ageHandler(event) {
+    this.setState({age: event.target.value})
+  }
+
+  emailHandler(event) {
+    this.setState({email: event.target.value})
+  }
+
+  idHandler(event) {
+    this.setState({id: event.target.value})
+  }
+
+  submitHandler(event) {
+    event.preventDefault()
+    
+    const newFriend = {
+      name: this.state.name,
+      age: this.state.age,
+      email: this.state.email
+    }
+
+    axios
+      .post(`http://localhost:5000/friends`, newFriend)
+      .then(response => {
+        this.setState({
+          friends: response.data
+        })
+      })
+  }
+
+ updateHandler(event) {
+    event.preventDefault()
+
+    const updatedFriend = {
+      name: this.state.name,
+      age: this.state.age,
+      email: this.state.email,
+      id: this.state.id
+    }
+ } 
+
   render() {
     return (
       <div>
@@ -34,14 +84,18 @@ class App extends React.Component {
             <FriendDetails key={friend.id} friend={friend} />
          ))}
         </div>
-        <form>
+        <form onSubmit={this.submitHandler.bind(this)}>
           <h2>Add Your Friends!</h2>
           <label>
-            <input type='text' placeholder={`Friend's Name`} /> <br />
-            <input type='text' placeholder={`Friend's Age`} /> <br />
-            <input type='text' placeholder={`Friend's E-Mail`} /> <br />
+            <input type='text' placeholder={`Friend's Name`} value={this.state.name} onChange={this.nameHandler.bind(this)} /> <br />
+            <input type='text' placeholder={`Friend's Age`}  value={this.state.age} onChange={this.ageHandler.bind(this)} /> <br />
+            <input type='text' placeholder={`Friend's E-Mail`} value={this.state.email} onChange={this.emailHandler.bind(this)} /> <br />
           </label>
-          <input type='submit' />
+          <input type='submit' value='submit' />
+        </form>
+        <form onSubmit={this.updateHandler.bind(this)}>
+          <input type='text' placeholder='ID to update' value={this.state.id} onChange={this.idHandler.bind(this)} />
+          <input type='submit' value='submit' />
         </form>
       </div>
 
@@ -50,12 +104,13 @@ class App extends React.Component {
 }
 
 function FriendDetails({ friend }) {
-  const { name, age, email } = friend;
+  const { name, age, email, id } = friend;
   return (
     <div className='friend-card'>
       <h2>{name}</h2>
       <div>Age: {age}</div>
       <div>E-mail: {email}</div>
+      <div>ID: {id}</div>
     </div>
   )
 } 
