e590bfdcf4cdb0fb169354298a88c68cf1bd7225
Vaeliwen <zaman@imbriaco.com>
2019-06-12 17:11:46 -0400

    Completed basic list, began form.

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import axios from 'axios'
+import axios from 'axios';
 
 
 
@@ -14,17 +13,52 @@ class App extends React.Component {
   }
 
 
+
+
   componentDidMount() {
     axios
     .get(`http://localhost:5000/friends`)
-        .then(response => this.setState({friends: response}))
-        .catch(err => console.log(err))
+        .then(response => {
+          this.setState({friends: response.data})
+        })
+        .catch(err => {
+          console.error('Server error', err)
+        });
   };
 
   render() {
-    return <div>Hello world</div>
-  }
+    return (
+      <div>
+        <div className='friends-list'>
+          {this.state.friends.map(friend => (
+            <FriendDetails key={friend.id} friend={friend} />
+         ))}
+        </div>
+        <form>
+          <h2>Add Your Friends!</h2>
+          <label>
+            <input type='text' placeholder={`Friend's Name`} /> <br />
+            <input type='text' placeholder={`Friend's Age`} /> <br />
+            <input type='text' placeholder={`Friend's E-Mail`} /> <br />
+          </label>
+          <input type='submit' />
+        </form>
+      </div>
 
+    )
+  }
 }
+
+function FriendDetails({ friend }) {
+  const { name, age, email } = friend;
+  return (
+    <div className='friend-card'>
+      <h2>{name}</h2>
+      <div>Age: {age}</div>
+      <div>E-mail: {email}</div>
+    </div>
+  )
+} 
+
 
 export default App;
