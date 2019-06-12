3ca4c6f1235085f43bae2adbcea97a2266e11ec9
Vaeliwen <zaman@imbriaco.com>
2019-06-12 15:54:12 -0400

    Implemented axios Promise setting state of friends to server-side array.

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      friends: ['test']
+    } 
+  }
+
+
+  componentDidMount() {
+    axios
+    .get(`http://localhost:5000/friends`)
+        .then(response => this.setState({friends: response}))
+        .catch(err => console.log(err))
+  };
+
+  render() {
+    return <div>Hello world</div>
+  }
+
 }
 
 export default App;
